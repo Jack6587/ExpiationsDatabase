0efce41e58bc62fb962abd5dfbb9375fcafe592b
Jack Klenke <klejt002@mymail.unisa.edu.au>
2024-09-09 14:23:02 +0930

    Just started the first task - the aim here was to create a controller and a view just to display the expiation categories. Nothing special at the moment, no search, just created an index action and view to display the category list

---
+++ Assig1/Controllers/ExpiationsController.cs
@@ -0,0 +1,22 @@
+﻿using Assig1.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Assig1.Controllers
+{
+    public class ExpiationsController : Controller
+    {
+        private readonly ExpiationsContext _context;
+
+        public ExpiationsController(ExpiationsContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var expiationCategores = _context.ExpiationCategories.ToList();
+
+            return View(expiationCategores);
+        }
+    }
+}

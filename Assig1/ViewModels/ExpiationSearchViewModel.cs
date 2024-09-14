f5080b9d380f540e005b4d03aeccedbe5f2e4dda
Jack Klenke <klejt002@mymail.unisa.edu.au>
2024-09-14 12:58:01 +0930

    Added elements to the view model

--- Assig1/ViewModels/ExpiationSearchViewModel.cs
+++ Assig1/ViewModels/ExpiationSearchViewModel.cs
@@ -1,6 +1,14 @@
-﻿namespace Assig1.ViewModels
+﻿using Assig1.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace Assig1.ViewModels
 {
     public class ExpiationSearchViewModel
     {
+        [Required(ErrorMessage = "You must provide a search term")]
+        public string SearchText { get; set; }
+        public int? SelectedCategoryId { get; set; }
+        public IEnumerable<ExpiationCategory> Categories { get; set; }
+        public IEnumerable<Expiation> Expiations { get; set; }
     }
 }

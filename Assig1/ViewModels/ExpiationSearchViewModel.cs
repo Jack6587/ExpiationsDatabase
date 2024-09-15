bd36f2e8ec9cd19fa7aa88af718a39f1c99b2dc3
Jack Klenke <klejt002@mymail.unisa.edu.au>
2024-09-15 14:28:28 +0930

    Think I'm going to change my search criteria to speeding categories + offences. Probably overthinking things, but at least I have the foundation established. Worth exploring regardless

--- Assig1/ViewModels/ExpiationSearchViewModel.cs
+++ Assig1/ViewModels/ExpiationSearchViewModel.cs
@@ -3,11 +3,12 @@ using System.ComponentModel.DataAnnotations;
 
 namespace Assig1.ViewModels
 {
-    public class ExpiationSearchViewModel
+    public class SpeedingCategoriesSearchViewModel
     {
         [Required(ErrorMessage = "You must provide a search term")]
         public string SearchText { get; set; }
         public int? SelectedCategoryId { get; set; }
-        public IEnumerable<ExpiationCategory> Categories { get; set; }
+        public IEnumerable<SpeedingCategory> SpeedingCategories { get; set; }
+        public IEnumerable<Offence> Offences {  get; set; }
     }
 }

b89033e202e6a06a5e3916790fbe972b4836b7d2
Jack Klenke <klejt002@mymail.unisa.edu.au>
2024-09-16 12:32:48 +0930

    Details model contains some information. Nothing crazy right now, just testing to see how things work

--- Assig1/ViewModels/Speeding_SpeedingDetail.cs
+++ Assig1/ViewModels/Speeding_SpeedingDetail.cs
@@ -2,5 +2,9 @@
 {
     public class Speeding_SpeedingDetail
     {
+        public string OffenceCode { get; set; }
+        public string Description { get; set; }
+        public int ExpiationFee { get; set; }
+        public int demeritPoints { get; set; }
     }
 }

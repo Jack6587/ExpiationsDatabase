7a330c759734c543f112566201cd8afcdb63b9f6
Jack Klenke <klejt002@mymail.unisa.edu.au>
2024-09-17 14:58:53 +0930

    Made a view model that reflects what is already in the controller. Now to implement it in the controller

---
+++ Assig1/ViewModels/DataBreakdownViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Assig1.ViewModels
+{
+    public class DataBreakdownViewModel
+    {
+        public string OffenceCode { get; set; }
+        public string Description { get; set; }
+        public DateTime IncidentStartDate { get; set; }
+        public decimal TotalFeeAmt { get; set; }
+    }
+}

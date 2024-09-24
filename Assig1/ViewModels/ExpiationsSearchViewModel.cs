c23cadbd6cc6e5b3a773b2612fe005d1a5067940
Jack Klenke <klejt002@mymail.unisa.edu.au>
2024-09-24 14:09:28 +0930

    Added some elements to the expiations view model to be displayed in the index

--- Assig1/ViewModels/ExpiationsSearchViewModel.cs
+++ Assig1/ViewModels/ExpiationsSearchViewModel.cs
@@ -1,6 +1,16 @@
-﻿namespace Assig1.ViewModels
+﻿using Assig1.Models;
+
+namespace Assig1.ViewModels
 {
     public class ExpiationsSearchViewModel
     {
+        public List<Expiation> Expiations { get; set; } = new List<Expiation>();
+
+        public int TotalExpiations { get; set; }
+        public int? MaxSpeed { get; set; }
+        public double? AverageSpeed { get; set; }
+        public decimal MaxBAC { get; set; }
+        public int? MaxFine { get; set; }
+        public double? AverageFine { get; set; }
     }
 }

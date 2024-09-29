39f25b5a5494babfebb2e1ca7c860c9ef54f81b2
Jack Klenke <klejt002@mymail.unisa.edu.au>
2024-09-29 23:34:19 +0930

    Starting to implement data annotations for expiation detail, just working through the detail models first before adding some to the view models

--- Assig1/ViewModels/Expiation_ExpiationDetail.cs
+++ Assig1/ViewModels/Expiation_ExpiationDetail.cs
@@ -1,13 +1,22 @@
 ﻿using Assig1.Models;
+using System.ComponentModel.DataAnnotations;
 
 namespace Assig1.ViewModels
 {
     public class Expiation_ExpiationDetail
     {
         public Expiation Expiation { get; set; }
+
+        [Display(Name = "Number of Expiations in Driver's State")]
         public int DriverStateCount { get; set; }
+
+        [Display(Name = "Number of Expiations in Same LSA Code Area")]
         public int LsaCount { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:F2}")]
         public double SpeedPercentile { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:F2}")]
         public double BacPercentile { get; set; }
     }
 }

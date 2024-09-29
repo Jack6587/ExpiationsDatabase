4a4c3bf3150c12b883cd570d18b6275756ba5308
Jack Klenke <klejt002@mymail.unisa.edu.au>
2024-09-29 23:54:28 +0930

    Theoretically finished adding annotations to the speeding detail model. Some of this is subject to change, especially considering there is still a week left until the due date; I am likely to look over and change or add more aggregates especially if I feel as though they are lacking. Finished for now, though

--- Assig1/ViewModels/Speeding_SpeedingDetail.cs
+++ Assig1/ViewModels/Speeding_SpeedingDetail.cs
@@ -1,14 +1,36 @@
-﻿namespace Assig1.ViewModels
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Assig1.ViewModels
 {
     public class Speeding_SpeedingDetail
     {
+        [Required]
+        [Display(Name = "Offence Code")]
         public string OffenceCode { get; set; }
+
+        [Display(Name = "Description")]
         public string Description { get; set; }
+
+        [Display(Name = "Expiation Fee")]
         public int? ExpiationFee { get; set; }
+
+        [Display(Name = "Demerit Points")]
         public int? DemeritPoints { get; set; }
+
+        [Display(Name = "Total Offences")]
         public int TotalOffences { get; set; }
+
+        [Display(Name = "Average Demerit Points In This Offence")]
+        [DisplayFormat(DataFormatString = "{0:F2}")]
         public double? AverageDemeritPoints { get; set; }
+
+        [Display(Name = "Average Fee Paid In This Offence")]
+        [DisplayFormat(DataFormatString = "{0:C}")]
         public double? AverageFeePaid { get; set; }
+
+        [Display(Name = "Highest Fee Paid In This Offence")]
+        [DisplayFormat(DataFormatString = "{0:C}")]
         public int? HighestFeePaid { get; set; }
     }
 }

367b2584cba1b5b5daf40b2ad7ff9de4e80fc42b
Jack Klenke <klejt002@mymail.unisa.edu.au>
2024-10-06 22:24:59 +1030

    Added annotations and formatting for expiations index

--- Assig1/ViewModels/ExpiationsSearchViewModel.cs
+++ Assig1/ViewModels/ExpiationsSearchViewModel.cs
@@ -16,12 +16,31 @@ namespace Assig1.ViewModels
         [Display(Name = "Search by Offence Code")]
         [StringLength(20, ErrorMessage = "Offence Code must not exceed 20 characters")]
         public string OffenceCode { get; set; }
+
+        [Display(Name = "Sort Order")]
         public string SortOrder { get; set; }
+
+        [Display(Name = "Total Expiations")]
         public int TotalExpiations { get; set; }
+
+        [Display(Name = "Maximum Speed")]
+        [DisplayFormat(DataFormatString = "{0:N0}")]
         public int? MaxSpeed { get; set; }
+
+        [Display(Name = "Average Speed")]
+        [DisplayFormat(DataFormatString = "{0:F2}")]
         public double? AverageSpeed { get; set; }
+
+        [Display(Name = "Maximum BAC")]
+        [DisplayFormat(DataFormatString = "{0:F3}")]
         public decimal? MaxBAC { get; set; }
+
+        [Display(Name = "Maximum Fine")]
+        [DisplayFormat(DataFormatString = "{0:C}")]
         public int? MaxFine { get; set; }
+
+        [Display(Name = "Average Fine")]
+        [DisplayFormat(DataFormatString = "{0:C}")]
         public double? AverageFine { get; set; }
         public int CurrentPage { get; set; }
         public int TotalPages { get; set; }
